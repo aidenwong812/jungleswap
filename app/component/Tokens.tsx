032d747185bfd73747ac7ff674f9148e2fdf6b8d
Tomas <mantoketo4@gmail.com>
2024-10-25 11:53:25 +1000

    Both swap

--- app/component/Tokens.tsx
+++ app/component/Tokens.tsx
@@ -1,21 +1,26 @@
 import { useState } from "react";
 import { RiArrowDownSFill } from "react-icons/ri";
 import Modal from "./Modal";
-const TokenButtons = ({ tokenStyle, setTokenStyle, style, tempTokenImage }: { tokenStyle: string, style: string, setTokenStyle: any, tempTokenImage: string }) => {
+const TokenButtons = ({ tokenStyle, setTokenStyle, style, tokenChain, setTokenChain, tempTokenImage }: { tokenStyle: string, style: string, setTokenStyle: any, tokenChain: string, setTokenChain: any, tempTokenImage: string }) => {
     const [isModalShow, setIsModalShow] = useState(false);
     const [tokenImage, setTokenImage] = useState(tempTokenImage);
     return (
         <>
             <div className="flex gap-1 items-center w-[30%]" onClick={() => { setIsModalShow(true) }}>
                 <img src={tokenImage} alt="tokenimage" />
-                {
-                    tokenStyle.length > 5 ?
-                        tokenStyle.slice(0, 4)
-                        : tokenStyle
-                }
+                <div className="flex flex-col items-center justify-center">
+                    {
+                        tokenStyle.length > 5 ?
+                            tokenStyle.slice(0, 4)
+                            : tokenStyle
+                    }
+                    <p className="text-xs">
+                    {`(${tokenChain})`}
+                    </p>
+                </div>
                 <RiArrowDownSFill className="size-9" />
             </div>
-            {isModalShow && <Modal setTokenStyle={setTokenStyle} style={style} setIsModalShow={setIsModalShow} setTokenImage={setTokenImage} />}
+            {isModalShow && <Modal setTokenStyle={setTokenStyle} style={style} setIsModalShow={setIsModalShow} setTokenImage={setTokenImage} setTokenChain={setTokenChain} />}
         </>
     )
 }
